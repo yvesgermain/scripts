0f57fb117d0fceda14c65dd7ace598673791e533
Yves Germain <34469127+yvesgermain@users.noreply.github.com>
2023-11-02 22:42:00 -0400

    Enabled printer logs

--- Good-to-have.ps1
+++ Good-to-have.ps1
@@ -68,7 +68,7 @@ Get-Content \\kruger.com\NETLOGON\tr\TrLogon.kix | Where-Object {
     $oldIsMember = $isMember
 }
 
-if ($case) {Remove-Variable case}
+if ($case) { Remove-Variable case }
 $tr = Get-Content \\kruger.com\NETLOGON\tr\TrLogon.kix | Where-Object {
     -not [String]::IsNullOrWhiteSpace($_) -and $_ -notlike "*;*" } | ForEach-Object {
     if ($_ -like "Function*" -or $_ -like "EndFunction") {
@@ -81,74 +81,74 @@ $tr = Get-Content \\kruger.com\NETLOGON\tr\TrLogon.kix | Where-Object {
             if ($_ -match "InGroup") {
                 $case = $_.split('"')[1]
             }
-            if ($_ -match "AddNewDisk"){
-                $string = $_.replace(')',"")
-                $server, $share, $scrap, $letter, $option1 = $string.split(",",  [StringSplitOptions]::RemoveEmptyEntries)
-                $server =   $server.replace('   AddNewDisk  ( "',"").replace('"',"").trim()
+            if ($_ -match "AddNewDisk") {
+                $string = $_.replace(')', "")
+                $server, $share, $scrap, $letter, $option1 = $string.split(",", [StringSplitOptions]::RemoveEmptyEntries)
+                $server = $server.replace('   AddNewDisk  ( "', "").replace('"', "").trim()
                 $folder = '\\' + $Server + '\' + $share.trim()
-                $letter = $letter.replace('"',"")
-                $option1= $option1.replace('"',"")
-                $_ | Select-Object @{name = "Letter" ; e = { $letter.replace(":","").trim() }},
-                @{name = "Group";e = {$case}},
-                @{name = "Path" ;e = {$Folder.replace('"',"")}},
-                @{name = "User"; e = { $user.replace('/user:',"") }},
-                @{name = "Password"; e = { $pwd.replace('/password:',"") }},
-                @{name= "Option1";e ={$option1.trim()}},
-                @{name ="Option2";e = {$option2}};
+                $letter = $letter.replace('"', "")
+                $option1 = $option1.replace('"', "")
+                $_ | Select-Object @{name = "Letter" ; e = { $letter.replace(":", "").trim() } },
+                @{name = "Group"; e = { $case } },
+                @{name = "Path" ; e = { $Folder.replace('"', "") } },
+                @{name = "User"; e = { $user.replace('/user:', "") } },
+                @{name = "Password"; e = { $pwd.replace('/password:', "") } },
+                @{name = "Option1"; e = { $option1.trim() } },
+                @{name = "Option2"; e = { $option2 } }
                 if ($letter -or $path) { Remove-Variable letter }
                 If ($folder) { Remove-Variable folder }
                 if ($pwd) { Remove-Variable pwd }
                 if ($user) { Remove-Variable user }
-                if ($option1) {Remove-Variable Option1}
-                if ($OPtion2) {Remove-Variable option2}
-                remove-variable String
+                if ($option1) { Remove-Variable Option1 }
+                if ($OPtion2) { Remove-Variable option2 }
+                Remove-Variable String
 
             }
             if ($_ -match 'AddDisk') {
-                $string = $_.replace(')',"")
-                $Server, $Share, $letter, $option1 = $string.split(",",  [StringSplitOptions]::RemoveEmptyEntries)
+                $string = $_.replace(')', "")
+                $Server, $Share, $letter, $option1 = $string.split(",", [StringSplitOptions]::RemoveEmptyEntries)
                 $Server = $server.split('"')[1].trim()
                 $Share = $Share.trim()
                 $folder = '\\' + $Server + '\' + $share + '\$user'
-                $letter = $letter.replace(":","").replace('"',"").trim()
-                $option1= $option1.replace('"',"")
-                $_ | Select-Object @{name = "Letter" ; e = { $letter.replace(":","") }},
-                @{name = "Group";e = {$case}},
-                @{name = "Path" ;e = {$Folder.replace('"',"")}},
-                @{name = "User or Group"; e = { $user.replace('/user:',"") }},
-                @{name = "Password"; e = { $pwd.replace('/password:',"") }},
-                @{name= "Option1";e ={$option1.trim()}},
-                @{name ="Option2";e = {$option2}};
+                $letter = $letter.replace(":", "").replace('"', "").trim()
+                $option1 = $option1.replace('"', "")
+                $_ | Select-Object @{name = "Letter" ; e = { $letter.replace(":", "") } },
+                @{name = "Group"; e = { $case } },
+                @{name = "Path" ; e = { $Folder.replace('"', "") } },
+                @{name = "User or Group"; e = { $user.replace('/user:', "") } },
+                @{name = "Password"; e = { $pwd.replace('/password:', "") } },
+                @{name = "Option1"; e = { $option1.trim() } },
+                @{name = "Option2"; e = { $option2 } }
                 if ($letter -or $path) { Remove-Variable letter }
                 If ($folder) { Remove-Variable folder }
                 if ($pwd) { Remove-Variable pwd }
                 if ($user) { Remove-Variable user }
-                if ($option1) {Remove-Variable Option1}
-                if ($OPtion2) {Remove-Variable option2}
-                remove-variable String
-        }
+                if ($option1) { Remove-Variable Option1 }
+                if ($OPtion2) { Remove-Variable option2 }
+                Remove-Variable String
+            }
             if ($_ -match "CASE ") {
-                $case = $_.Replace('CASE $USER = "',"").replace('"',"")
+                $case = $_.Replace('CASE $USER = "', "").replace('"', "")
             }
             if ($_ -match "use ") {
-                $string = $_.trim();
+                $string = $_.trim()
                 $scrap, $letter, $path = $string.split(" "); $letter = ($letter -replace ("use " , "")).trim()
-                if ($string -like "*\\*") {$folder , $user, $pwd = $Path.split(" ", [StringSplitOptions]::RemoveEmptyEntries)}
-                if ($string -notlike "*\\*") {$option1, $option2  = $path.trim().split(" ", [StringSplitOptions]::RemoveEmptyEntries)}
-                $_ | Select-Object @{name = "Letter" ; e = { $letter.replace(":","") }},
-                @{name = "Group";e = {$case}},
-                @{name = "Path" ;e = {$Folder.replace('"',"")}},
-                @{name = "User"; e = { $user.replace('/user:',"") }},
-                @{name = "Password"; e = { $pwd.replace('/password:',"") }},
-                @{name= "Option1";e ={$option1.trim()}},
-                @{name ="Option2";e = {$option2}};
+                if ($string -like "*\\*") { $folder , $user, $pwd = $Path.split(" ", [StringSplitOptions]::RemoveEmptyEntries) }
+                if ($string -notlike "*\\*") { $option1, $option2 = $path.trim().split(" ", [StringSplitOptions]::RemoveEmptyEntries) }
+                $_ | Select-Object @{name = "Letter" ; e = { $letter.replace(":", "") } },
+                @{name = "Group"; e = { $case } },
+                @{name = "Path" ; e = { $Folder.replace('"', "") } },
+                @{name = "User"; e = { $user.replace('/user:', "") } },
+                @{name = "Password"; e = { $pwd.replace('/password:', "") } },
+                @{name = "Option1"; e = { $option1.trim() } },
+                @{name = "Option2"; e = { $option2 } }
                 if ($letter -or $path) { Remove-Variable letter , path }
                 If ($folder) { Remove-Variable folder }
                 if ($pwd) { Remove-Variable pwd }
                 if ($user) { Remove-Variable user }
-                if ($option1) {Remove-Variable Option1}
-                if ($OPtion2) {Remove-Variable option2}
-                remove-variable String
+                if ($option1) { Remove-Variable Option1 }
+                if ($OPtion2) { Remove-Variable option2 }
+                Remove-Variable String
             }
         }
     }
@@ -159,13 +159,13 @@ $ho = convert-kix2Drive \\kruger.com\NETLOGON\ho\kixtart.kix
 
 $CB = Get-Content \\kruger.com\NETLOGON\cb\LoginCB.kix | Where-Object {
     -not [String]::IsNullOrWhiteSpace($_) -and $_ -notlike "*;*" } | ForEach-Object {
-        $case = "AnyOU"
+    $case = "AnyOU"
     if ($_ -match "use ") { $letter, $path = $_.replace('"', "").split(":"); $letter = $letter.replace("use " , "").trim() }
     if ($_ -match "If INGROUP") { $isMember = $_.split('"')[1] }  else { $isMember = $null }
     if ($IsMember -eq $null -and $oldIsMember -ne $null) {
-        $_ | Select-Object @{name = "OU" ; e = { $case }},
+        $_ | Select-Object @{name = "OU" ; e = { $case } },
         @{name = "Letter"; e = { $letter } },
-        @{ name = "Path" ; e = { $path.trim()  } },
+        @{ name = "Path" ; e = { $path.trim() } },
         @{ name = "Group"; e = { $OldIsMember } }
     }
     $oldIsMember = $isMember
@@ -182,3 +182,27 @@ $ET = convert-kix2Drive \\kruger.com\NETLOGON\kk\kkLogin_et.kix
 $TU = convert-kix2Drive \\kruger.com\NETLOGON\kk\kklogin_TU.kix
 
 $ho + $CB + $ls + $PD + $PB + $ET + $TU | Format-Table -AutoSize
+
+####################  PRINTER STUFF ********************
+Get-ADGroupMember "KR Print Spooler Disable Exceptions" | ForEach-Object -Parallel {
+    $filter = @{LogName = "Microsoft-Windows-PrintService/Operational"; id = "307" }
+    $name = $_.name
+    (Get-WinEvent -ComputerName $name -FilterHashtable $filter)[0..5] | Select-Object @{name = "Server"; e = { $name } },
+    @{name = "User"; e = { $_.Properties[2].value } },
+    @{name = "Printer"; e = { $_.Properties[4].value } }
+}
+
+
+######################## Enabled Printer Logging ###################################
+Get-ADGroupMember "KR Print Spooler Disable Exceptions" | ForEach-Object {
+    Invoke-Command -ComputerName $_.name -ScriptBlock { 
+        $log = New-Object System.Diagnostics.Eventing.Reader.EventLogConfiguration "Microsoft-Windows-PrintService/Operational" 
+        $log.IsEnabled = $True
+        $log.SaveChanges()
+    }
+}
+######################## Logon of administrator's account ##########################
+$servers | ForEach-Object -Parallel {
+    $date = (Get-Date).AddMonths(-1) 
+    Get-WinEvent -ComputerName $_.name -FilterHashtable @{ LogName = 'security'; StartTime = $Date; Id = '4672', '4648'; data = "administrator" }
+} | Tee-Object -Variable out
